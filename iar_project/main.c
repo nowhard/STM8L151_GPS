88d8a1b215ed28c552dc2ba33cb993b2e2bb7569
nowhard <nowhard@mail.ru>
2017-05-26 15:49:33 +0300

    Начало реализации прерываний

--- iar_project/main.c
+++ iar_project/main.c
@@ -7,15 +7,18 @@
 void Keys_Init(void);
 void EXTI_Init(void);
 void ADC_Bat_Init(void);
-void SPI_Accel_Init(void);
-
+void SPI_Slave_Init(void);
+void RTC_WakeUp_Init(void);
 
 void main(void)
 {
   Keys_Init();
   EXTI_Init();
-  SPI_Accel_Init();
+  RTC_WakeUp_Init();
+  SPI_Slave_Init();
   ADC_Bat_Init();
+  
+  enableInterrupts();
   while (1)
   {
     //GPIO_ToggleBits(LED_GPIO_PORT, LED_GPIO_PINS);
@@ -38,11 +41,45 @@ void EXTI_Init(void)
     EXTI_SetPinSensitivity((EXTI_Pin_TypeDef)ACCEL_INT_EXTI, EXTI_Trigger_Falling);
 }
 
-void SPI_Accel_Init(void)
+void SPI_Slave_Init(void)
 {
+  SPI_Init(SPI1,SPI_FirstBit_MSB,SPI_BaudRatePrescaler_2,
+           SPI_Mode_Slave,SPI_CPOL_High,SPI_CPHA_1Edge,
+           SPI_Direction_2Lines_FullDuplex,SPI_NSS_Hard,0);
+  
+  
+  
 }
 
 void ADC_Bat_Init(void)
 {
 }
 
+void RTC_WakeUp_Init(void)
+{
+   CLK_PeripheralClockConfig(CLK_Peripheral_RTC, ENABLE);
+   
+      //Настраиваем RTC
+   RTC_WakeUpClockConfig(RTC_WakeUpClock_RTCCLK_Div16); //time_step = 488uS
+   RTC_ITConfig(RTC_IT_WUT, ENABLE);
+
+ 
+  // CFG->GCR |= CFG_GCR_AL; //Поднимаем флаг AL
+   RTC_SetWakeUpCounter(2049);//Прерывание через каждую секунду
+   RTC_WakeUpCmd(ENABLE);
+
+ //  halt();по команде SIM800
+}
+
+INTERRUPT_HANDLER(EXTI3_Accel_IRQHandler, 11)
+{
+
+ EXTI_ClearITPendingBit (EXTI_IT_Pin3);
+}
+
+
+INTERRUPT_HANDLER(RTC_Wakeup_IRQHandler, 4)
+{
+
+// EXTI_ClearITPendingBit (EXTI_IT_Pin3);
+}
